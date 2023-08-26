c6f96629b947ca90edbfa50605d3eee424582591
Omar <omarsan786@gmail.com>
2023-08-25 23:51:12 -0400

    fixing about section mobile formatting

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="pt-[120px] md:pt-0 w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="pt-[250px] pb-[150px] sm:pt-0 sm:pb-0 md:pt-0 w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-3">
